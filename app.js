c3f9dd624b0bb3471dfe848eaf64e2624bba7b81
Charly Lersteau <645647+charlyisidore@users.noreply.github.com>
2023-06-27 23:58:40 -0700

    Remove: useless code

--- app.js
+++ app.js
@@ -3,12 +3,6 @@ App({
   onLaunch() {
     // https://developers.weixin.qq.com/miniprogram/dev/api/base/update/UpdateManager.html
     const updateManager = wx.getUpdateManager();
-
-    updateManager.onCheckForUpdate((res) => {
-      // 请求完新版本信息的回调
-      console.log(res.hasUpdate);
-    });
-
     updateManager.onUpdateReady(() => {
       wx.showModal({
         title: '更新提示',
@@ -20,10 +14,6 @@ App({
           }
         },
       });
-    });
-
-    updateManager.onUpdateFailed(() => {
-      // 新版本下载失败
     });
   }
 })
