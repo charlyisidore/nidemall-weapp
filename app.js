366e44e79860084b0910f7309f45e17185b4bbc3
Charly Lersteau <645647+charlyisidore@users.noreply.github.com>
2023-06-27 18:15:10 -0700

    Add: app update manager

--- app.js
+++ app.js
@@ -1,2 +1,29 @@
 // app.js
-App({})
+App({
+  onLaunch() {
+    // https://developers.weixin.qq.com/miniprogram/dev/api/base/update/UpdateManager.html
+    const updateManager = wx.getUpdateManager();
+
+    updateManager.onCheckForUpdate((res) => {
+      // 请求完新版本信息的回调
+      console.log(res.hasUpdate);
+    });
+
+    updateManager.onUpdateReady(() => {
+      wx.showModal({
+        title: '更新提示',
+        content: '新版本已经准备好，是否重启应用？',
+        success: (res) => {
+          if (res.confirm) {
+            // 新的版本已经下载好，调用 applyUpdate 应用新版本并重启
+            updateManager.applyUpdate();
+          }
+        },
+      });
+    });
+
+    updateManager.onUpdateFailed(() => {
+      // 新版本下载失败
+    });
+  }
+})
